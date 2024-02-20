c4ef339dd2535049fc600b7d3b43bf778e2bf35e
Hafsa Mehmood <hafsaayyhere@gmail.com>
2024-02-20 14:08:43 -0800

    Main-Code

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -1,6 +1,4 @@
 import 'package:StunAi/Widgets/bottom_navbar.dart';
-import 'package:StunAi/Widgets/card.dart';
-import 'package:StunAi/Widgets/textfield.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -18,29 +16,108 @@ class _HomeScreenState extends State<HomeScreen> {
   TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-  
-  
     return Scaffold(
       appBar: AppBar(
         title: const Text('StunAi'),
         centerTitle: true,
       ),
-      bottomNavigationBar: BottomnavBar(current_index: index,),
-      
+      bottomNavigationBar: BottomnavBar(
+        current_index: index,
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
             Center(
-              child: CharacterCard(),
+              child: Container(
+                margin: const EdgeInsets.all(5.0),
+                child: Card(
+                  elevation: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Stack(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.9,
+                              height: MediaQuery.of(context).size.height * 0.6,
+                              clipBehavior: Clip.antiAlias,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Image.asset(
+                                'assets/profile.jpg',
+                                height: 135,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Positioned(
+                              left: 0,
+                              child: CircleAvatar(
+                                backgroundColor: Colors.white,
+                                child: IconButton(
+                                  icon: const Icon(Icons.favorite),
+                                  onPressed: () async {},
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              right: 0,
+                              child: CircleAvatar(
+                                backgroundColor: Colors.white,
+                                child: IconButton(
+                                  icon: const Icon(Icons.download),
+                                  onPressed: () async {},
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
             const SizedBox(
               height: 20,
             ),
-             TextFieldButton(textEditingController: textEditingController),
+            Material(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: TextField(
+                        style: const TextStyle(color: Colors.black54),
+                        controller: textEditingController,
+                        onSubmitted: (value) async {},
+                        decoration: const InputDecoration.collapsed(
+                          hintText: "Insert Text To generate Image",
+                          hintStyle: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () async {},
+                      icon: const Icon(
+                        Icons.send,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
-
     );
   }
 }
